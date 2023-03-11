bf1906eaca959a6c56dedad1bfea16c0663cf2b6
Inzi27 <Ic4cool@gmail.com>
2023-03-11 20:50:01 +0530

    initial commit

---
+++ src/test/java/io/inzi/springsecuritynew/SpringSecurityNewApplicationTests.java
@@ -0,0 +1,13 @@
+package io.inzi.springsecuritynew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
